feec3dc4157b16766a0eba024da925acaa78f4a2
samiraznag50-beep <samiraznag50@gmail.com>
2026-04-20 23:25:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-worker: python bot.py
+import telebot
+import os
+
+# Hna k-n-tjib l-Token mn Railway Variables
+TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hana khdam! Chno bghiti d-dir?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Rk sifti lia: {message.text}")
+
+print("Bot is starting...")
+bot.infinity_polling()
+
