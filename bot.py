07e3ee104c5e356281dda6486f8abd58ab445bbf
samiraznag50-beep <samiraznag50@gmail.com>
2026-04-21 00:46:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ CHOT_TOKEN = os.getenv('BOT_TOKEN')
 GEMINI_KEY = os.getenv('GEMINI_API_KEY')
 
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 bot = telebot.TeleBot(CHOT_TOKEN)
 
 # Function bach n-sowbo s-sout (Voiceover)
